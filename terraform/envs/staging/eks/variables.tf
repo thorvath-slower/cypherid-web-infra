18a816afd6ef631cb2da97d359e3e050295bbd16
jsims-slower <jsims-slower@users.noreply.github.com>
2026-04-22 00:50:20 +0000

    commit from fogg_ci -- ran terraform fmt and pushed

--- terraform/envs/staging/eks/variables.tf
+++ terraform/envs/staging/eks/variables.tf
@@ -2,7 +2,7 @@ locals {
   owner_roles = [
     data.terraform_remote_state.access-management.outputs.gh_actions_executor_role.name, # TODO: Not sure if it is required to prevent Unauthorized in Github Actions
     #"poweruser",                                                                         # TODO - Where are these created, and are they necessary? Appear to be deployed via terraform from 'shared-infra'. For now, manually created a 'poweruser' IAM role that matches what is in the CZI dev account.
-    "AWSReservedSSO_AWSAdministratorAccess_f655aa408ec10f58",                            # SSO role used when locally applying terraform with an SSO profile
+    "AWSReservedSSO_AWSAdministratorAccess_f655aa408ec10f58", # SSO role used when locally applying terraform with an SSO profile
     # "gha-seqtoid", // Role used by GH Actions for applying terraform (cypherid-infra & cypherid-web-infra)
     # "okta-czi-admin",
     # "tfe-si"
