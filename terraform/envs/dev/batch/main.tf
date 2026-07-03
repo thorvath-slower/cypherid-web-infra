f36dce7f1e65c978e431bae053fda16f6c988ab0
Thomas Horvath <thomash@slower.ai>
2026-07-03 17:54:42 -0400

    czid-366: author dev batch + individual-attr stacks (env parity)
    Dev was the only web-infra env missing the batch and individual-attr
    stacks; staging and prod both carry them. Mirror them into dev so all
    non-sandbox envs are parity (#366 / WBS 20014).
    
    - dev/batch: verbatim staging main.tf (env-driven, no staging hardcodes);
      terraform.tf points at the dev account (491013321714), idseq-dev profile,
      tfstate-491013321714-test backend, and the dev ecs + cloud-env remote
      states.
    - dev/individual-attr: mirrors staging (shared modules/individual-attr),
      aws_account_id resolves to var.aws_accounts.idseq-dev.
    - .checkov.baseline: union-append the two inherited idseq-batch findings
      (CKV_AWS_111, CKV_AWS_356) + CKV_TF_1 that already exist verbatim on
      staging/batch; no NEW finding classes.
    
    Offline-validated: terraform fmt + init -backend=false + validate (both
    stacks Success). NOT applied (no AWS).

---
+++ terraform/envs/dev/batch/main.tf
@@ -0,0 +1,189 @@
+data "aws_region" "current" {}
+
+resource "aws_batch_job_queue" "idseq-himem" {
+  name                 = "idseq-${var.env}-himem"
+  state                = "ENABLED"
+  priority             = 20
+  compute_environments = [aws_batch_compute_environment.idseq_244GB_32CPU.arn]
+}
+
+resource "aws_batch_job_queue" "idseq-lomem" {
+  name                 = "idseq-${var.env}-lomem"
+  state                = "ENABLED"
+  priority             = 20
+  compute_environments = [aws_batch_compute_environment.idseq_122GB_16CPU.arn]
+}
+
+module "idseq-batch" {
+  source      = "../../../modules/aws-iam-instance-profile-v0.104.2" # cztack v0.104.2
+  name_prefix = "idseq-batch-${var.env}"
+}
+
+data "aws_iam_policy_document" "idseq-batch" {
+  statement {
+    actions = [
+      "ecs:CreateCluster",
+      "ecs:DeregisterContainerInstance",
+      "ecs:DiscoverPollEndpoint",
+      "ecs:Poll",
+      "ecs:RegisterContainerInstance",
+      "ecs:StartTelemetrySession",
+      "ecs:UpdateContainerInstancesState",
+      "ecs:Submit*",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_public_references}",
+      "arn:aws:s3:::${var.s3_bucket_samples}",
+      # We use staging batch to process samples in development
+      # "arn:aws:s3:::idseq-samples-development",
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_public_references}/*",
+      "arn:aws:s3:::${var.s3_bucket_samples}/*",
+      "arn:aws:s3:::${var.s3_bucket_secrets}/*",
+      # We use staging batch to process samples in development
+      # "arn:aws:s3:::idseq-samples-development/*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:DeleteObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_samples}/*",
+      # We use staging batch to process samples in development
+      # "arn:aws:s3:::idseq-samples-development/*",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "idseq-batch" {
+  name   = "idseq-batch-${var.env}"
+  role   = module.idseq-batch.role_name
+  policy = data.aws_iam_policy_document.idseq-batch.json
+}
+
+data "aws_iam_policy_document" "aws_batch_service_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["batch.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "aws_batch_service_role" {
+  name               = "aws_batch_service_role-${var.env}"
+  assume_role_policy = data.aws_iam_policy_document.aws_batch_service_role.json
+}
+
+# module "orgwide-secrets" {
+#   source    = "git@github.com:chanzuckerberg/shared-infra//terraform/modules/aws-iam-policy-orgwide-secrets?ref=v0.66.0"
+#   role_name = aws_iam_role.aws_batch_service_role.name
+# }
+
+resource "aws_iam_role_policy_attachment" "aws_batch_service_role" {
+  role       = aws_iam_role.aws_batch_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
+}
+
+resource "random_id" "batch" {
+  keepers = {
+    # Generate a new id each time we switch to a new AMI id
+    # TODO(elopez): temporarily commenting out due to ECS metadata endpoint throttling
+    # image_id          =  module.images.czi_amazon2_ecs
+    security_group_id = data.terraform_remote_state.ecs.outputs.security_group_id
+  }
+
+  byte_length = 8
+}
+
+resource "aws_batch_compute_environment" "idseq_244GB_32CPU" {
+  compute_environment_name = "idseq_${var.env}_244GB_32CPU_${random_id.batch.hex}"
+
+  compute_resources {
+    instance_role = module.idseq-batch.profile_arn
+
+    instance_type = [
+      "r5d.8xlarge",
+    ]
+
+    # ec2_key_pair       = "idseq-${var.env}"
+    max_vcpus          = 1024
+    min_vcpus          = 0
+    security_group_ids = [random_id.batch.keepers.security_group_id]
+    subnets            = data.terraform_remote_state.cloud-env.outputs.private_subnets
+    type               = "EC2"
+    tags               = var.tags
+
+    # image_id           =  random_id.batch.keepers.image_id
+  }
+
+  service_role = aws_iam_role.aws_batch_service_role.arn
+  type         = "MANAGED"
+  depends_on   = [aws_iam_role_policy_attachment.aws_batch_service_role]
+
+  lifecycle {
+    ignore_changes = [
+      compute_resources[0].desired_vcpus,
+    ]
+  }
+}
+
+resource "aws_batch_compute_environment" "idseq_122GB_16CPU" {
+  compute_environment_name = "idseq_${var.env}_122GB_16CPU_${random_id.batch.hex}"
+
+  compute_resources {
+    instance_role = module.idseq-batch.profile_arn
+
+    instance_type = [
+      "r5d.4xlarge",
+    ]
+
+    # ec2_key_pair       = "idseq-${var.env}"
+    max_vcpus          = 1024
+    min_vcpus          = 0
+    security_group_ids = [random_id.batch.keepers.security_group_id]
+    subnets            = data.terraform_remote_state.cloud-env.outputs.private_subnets
+    type               = "EC2"
+
+    # image_id           =  random_id.batch.keepers.image_id
+  }
+
+  service_role = aws_iam_role.aws_batch_service_role.arn
+  type         = "MANAGED"
+  depends_on   = [aws_iam_role_policy_attachment.aws_batch_service_role]
+
+  lifecycle {
+    ignore_changes = [
+      compute_resources[0].desired_vcpus,
+    ]
+  }
+}
